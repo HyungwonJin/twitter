d84ade83c629ed3d59d0905d437b3bb6aadbb10a
HyungwonJin <hwjin92@gmail.com>
2021-08-28 00:49:08 +0900

    feat: immer 라이브러리를 사용해서 불변성을 간단히 구현함

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -91,64 +91,95 @@ const dummyComment = (data) => ({
         nickname: 'Doraemon',
     },
 })
-
+// reducer란 이전 상태를 액션을 통해 다음 상태로 만들어내는 함수(불변성은 지키면서)
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_POST_REQUEST:
-            return {
-                ...state,
-                addPostLoading: true,
-                addPostDone: false,
-                addPostError: null,
-            }
+    return produce(state, (draft) => { // state가 이름이 draft로 바뀜
+        switch (action.type) {
+            case ADD_POST_REQUEST:
+                draft.addPostLoading = true;
+                draft.addPostDone = false;
+                draft.addPostError = null;
+                break;
+            // return {
+            //     ...state,
+            //     addPostLoading: true,
+            //     addPostDone: false,
+            //     addPostError: null,
+            // }
 
-        case ADD_POST_SUCCESS:
-            return {
-                ...state,
-                mainPosts: [dummyPost(action.data), ...state.mainPosts],
-                // 추가 되는 게시물을 앞에 입력해야 최상단에 위치함
-                addPostLoading: false,
-                addPostDone: true,
-            }
+            case ADD_POST_SUCCESS:
+                draft.addPostLoading = false;
+                draft.addPostDone = true;
+                draft.mainPosts.unshift(dummyPost(action.data));
+                // draft.mainPosts = [dummyPost(action.data), ...state.mainPosts];
+                break;
 
-        case ADD_POST_FAILURE:
-            return {
-                ...state,
-                addPostLoading: false,
-                addPostError: action.error,
-            }
+            case ADD_POST_FAILURE:
+                draft.addPostLoading = false;
+                draft.addPostError = action.error;
+                break;
 
-        case ADD_COMMENT_REQUEST:
-            return {
-                ...state,
-                addCommentLoading: true,
-                addCommentDone: false,
-                addCommentError: null,
-            }
+            case REMOVE_POST_REQUEST:
+                draft.removePostLoading = true;
+                draft.removePostDone = false;
+                draft.removePostError = null;
+                break;
 
-        case ADD_COMMENT_SUCCESS: {
-            const postIndex = state.mainPosts.findIndex((v) => v.id === action.data.postId);
-            const post = { ...state.mainPosts[postIndex] };
-            post.Comments = [dummyComment(action.data.content), ...post.Comments];
-            const mainPosts = [...state.mainPosts];
-            mainPosts[postIndex] = post;
-            return {
-                ...state,
-                mainPosts,
-                addCommentLoading: false,
-                addCommentDone: true,
+            case REMOVE_POST_SUCCESS:
+                draft.removePostLoading = false;
+                draft.removePostDone = true;
+                draft.mainPosts = draft.mainPosts.filter((v) => v.id !== action.data);
+                break;
+            // return {
+            //     ...state,
+            //     mainPosts: state.mainPosts.filter((v) => v.id !== action.data),
+            //     // 추가 되는 게시물을 앞에 입력해야 최상단에 위치함
+            //     removePostLoading: false,
+            //     removePostDone: true,
+            // }
+
+            case REMOVE_POST_FAILURE:
+                draft.removePostLoading = false;
+                draft.removePostError = action.error;
+                break;
+
+            case ADD_COMMENT_REQUEST:
+                draft.addCommentLoading = true;
+                draft.addCommentDone = false;
+                draft.addCommentError = null;
+                break;
+
+            case ADD_COMMENT_SUCCESS: {
+                const post = draft.mainPosts.find((v) => v.id === action.data.postId);// 원하는 post를 찾아서
+                post.Comments.unshift(dummyComment(action.data.content)); // 제일 앞에 게시글을 추가
+                draft.addCommentLoading = false;
+                draft.addCommentDone = true;
+                break;
+                // const postIndex = state.mainPosts.findIndex((v) => v.id === action.data.postId);
+                // const post = { ...state.mainPosts[postIndex] };
+                // post.Comments = [dummyComment(action.data.content), ...post.Comments];
+                // const mainPosts = [...state.mainPosts];
+                // mainPosts[postIndex] = post;
+                // return {
+                //     ...state,
+                //     mainPosts,
+                //     addCommentLoading: false,
+                //     addCommentDone: true,
+                // }
             }
+            case ADD_COMMENT_FAILURE:
+                draft.addCommentLoading = false;
+                draft.addCommentError = action.error;
+                break;
+
+            default:
+                break;
+            // return state
         }
-        case ADD_COMMENT_FAILURE:
-            return {
-                ...state,
-                addCommentLoading: false,
-                addCommentError: action.error,
-            }
 
-        default:
-            return state
-    }
+    })
+
+
 }
 
 export default reducer;
