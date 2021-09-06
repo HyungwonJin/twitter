935c3b9a927a63afc7e4f8706569e175d45bf0ad
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-09-06 12:51:49 +0900

    feat: Setting for SSR with Next.js

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
+import { END } from 'redux-saga';
 
 import AppLayout from "../components/AppLayout";
 import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
 import { LOAD_POST_REQUEST } from '../reducers/post';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
+import wrapper from '../store/configureStore';
 
 
 const Home = () => {
@@ -18,15 +20,6 @@ const Home = () => {
             alert(retweetError)
         }
     }, [retweetError])
-
-    useEffect(() => {
-        dispatch({
-            type: LOAD_MY_INFO_REQUEST,
-        })
-        dispatch({
-            type: LOAD_POST_REQUEST,
-        });
-    }, []);
 
     useEffect(() => {
         function onScroll() {
@@ -55,5 +48,16 @@ const Home = () => {
         </AppLayout>
     )
 }
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req }) => { // 이 코드가 있으면 서버 쪽에서 SSR을 함
+    store.dispatch({
+        type: LOAD_MY_INFO_REQUEST,
+    });
+    store.dispatch({
+        type: LOAD_POST_REQUEST,
+    });
+    store.dispatch(END);
+    await store.sagaTask.toPromise();
+})
 
 export default Home;
--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,25 +1,26 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddelware from "redux-saga";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
 
-const configureStore = () => {
-    const sagaMiddleware = createSagaMiddelware();
+const configureStore = (context) => {
+    console.log(context);
+    const sagaMiddleware = createSagaMiddleware();
     const middlewares = [sagaMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
         ? compose(applyMiddleware(...middlewares))
-        : composeWithDevTools(applyMiddleware(...middlewares))
+        : composeWithDevTools(
+            applyMiddleware(...middlewares),
+        );
     const store = createStore(reducer, enhancer);
-    store.sageTask = sagaMiddleware.run(rootSaga);
+    store.sagaTask = sagaMiddleware.run(rootSaga);
     return store;
-}
+};
 
-const wrapper = createWrapper(configureStore, {
-    debug: process.env.NODE_ENV === 'development',
-});
+const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
 
 export default wrapper;
